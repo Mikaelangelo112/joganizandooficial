e79e6f8fe0af489b90af359b4a6c8a52708a333a
mikaelangelo112 <mikaelangelo112@gmail.com>
2018-11-11 00:39:57 -0300

    pronto pra programa tudo configurado

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp');
+var sass = require('gulp-ruby-sass');
+var watch = require('gulp-watch');
+
+//task para o sass
+gulp.task('sass',function(){
+	return sass('./sass/**/*.sass').pipe(gulp.dest('./css'))
+
+});
+gulp.task('watch',function(){
+	gulp.watch('./sass/**/*.sass', ['sass']);
+});
+//task default gulp
+gulp.task('default', ['sass','watch']);
